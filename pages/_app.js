88610a9c1ffa32c3d2ae9ae68f60076d0270d34a
Christopher Snowhill <kode54@gmail.com>
2022-01-15 16:08:35 -0800

    Fixed theme styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider } from '@mui/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../src/theme';
 
